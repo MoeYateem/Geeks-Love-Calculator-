8daca39b3942483f531fe98c1f18ebbfbfaff910
MoeYateem <mohamad.yateem@lau.edu>
2022-02-17 04:20:54 +0200

    Final(Nshalla)

--- app/src/main/java/com/example/geekslovecalculator/MainActivity.java
+++ app/src/main/java/com/example/geekslovecalculator/MainActivity.java
@@ -14,8 +14,12 @@ import android.widget.Toast;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    int position;
     Spinner spinner;
     ImageView imgView;
+    boolean selected[] = new boolean[9];
+    int[] numbers = new int[9];
+
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,15 +30,16 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         ad.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(ad);
         spinner.setOnItemSelectedListener(this);
-
     }
+
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
         String lang = spinner.getSelectedItem().toString();
         if(imgView != null){
             imgView.setImageResource(0);
         }
-
+        selected[i] = true;
+        position = i;
         imgView = (ImageView) findViewById(R.id.i1);
 
         if(lang.equals("JAVA")){
@@ -64,11 +69,24 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         else{
             imgView.setImageResource(0);
         }
+        generate();
+        fillTable(adapterView, numbers);
     }
+
+
 
     @Override
     public void onNothingSelected(AdapterView<?> adapterView) {
 
+    }
+    public void generate(){
+        TextView text = findViewById(R.id.output);
+        EditText input = findViewById(R.id.name);
+        Random random = new Random();
+        int perc = random.nextInt(100);
+
+        text.setText(Integer.toString(perc)+"%");
+        numbers[position] = perc;
     }
     public void generate(View v){
         TextView text = findViewById(R.id.output);
@@ -77,9 +95,24 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         int perc = random.nextInt(100);
 
         text.setText(Integer.toString(perc)+"%");
+        numbers[position] = perc;
+    }
 
+    public void fillTable(AdapterView parent, int[] percentages){
+        TextView vw = findViewById(R.id.table);
+        StringBuilder txt = new StringBuilder();
+        String lang;
 
+        for(int i = 0; i < 8; i++){
+            //lang = "testing";
+            lang = parent.getItemAtPosition(i).toString();
+            for(int j = 0; j <= 16 - lang.length(); j++){
+                lang += " ";
+            }
+            if(selected[i] && percentages[i]!= 0) txt.append("love result of \t\t\t\t" + lang + "\t\t is:" +percentages[i] + "\n");
+        }
 
+        vw.setText(txt.toString());
     }
 
 
